a12b92bd549227b25f6d47d05cc0bf5a663c437a
minorius <minorius.wts@gmail.com>
2017-07-02 23:48:35 +0300

    add multithreading

--- app/src/main/java/com/minorius/watchertube/gson/duration/Item.java
+++ app/src/main/java/com/minorius/watchertube/gson/duration/Item.java
@@ -4,7 +4,7 @@ package com.minorius.watchertube.gson.duration;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-public class Item {
+public class ItemDuration {
 
     @SerializedName("kind")
     @Expose
